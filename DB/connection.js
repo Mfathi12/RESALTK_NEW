0cc726a3f7199e898b3f77b5b1aadd88bf2f3512
Mfathi12 <mariamfathi17@gmail.com>
2025-09-25 14:52:14 +0300

    fix: vercel deployment

--- DB/connection.js
+++ DB/connection.js
@@ -8,7 +8,7 @@ export const connectDB =async ()=>{
         console.error('MongoDB connection failed:', error.message);
     }
 } */
-
+/* 
     import mongoose from "mongoose";
 
 let isConnected = null; // عشان نحتفظ بالـ connection global
@@ -30,6 +30,31 @@ export const connectDB = async () => {
     console.log(" MongoDB connected successfully");
   } catch (error) {
     console.error(" MongoDB connection failed:", error.message);
+    throw new Error("Database connection failed");
+  }
+};
+ */
+
+import mongoose from "mongoose";
+
+let isConnected = null; // عشان نحتفظ بالـ connection global
+
+export const connectDB = async () => {
+  if (isConnected) {
+    console.log("✅ Using existing MongoDB connection");
+    return;
+  }
+
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      serverSelectionTimeoutMS: 5000, // timeout أسرع
+    });
+
+    isConnected = conn.connections[0].readyState; // 1 = connected
+    console.log("✅ MongoDB connected successfully");
+  } catch (error) {
+    console.error("❌ MongoDB connection failed:", error.message);
     throw new Error("Database connection failed");
   }
 };
